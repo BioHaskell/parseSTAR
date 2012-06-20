64b030e7738ecbfa55927034299d49beaea94507
Michal J. Gajda <mjgajda@mail.google.com>
2012-06-20 03:29:54 +0200

    TestConverter: error message and filename in same line.

--- test/TestConverter.hs
+++ test/TestConverter.hs
@@ -4,12 +4,16 @@ import Data.STAR.Parser(parseFile)
 import Data.STAR.Type
 import System.Environment(getArgs)
 import Data.Binary(encodeFile)
-import System.IO(hPrint, hPutStrLn, stderr)
+import System.IO(hPrint, hPutStr, hPutStrLn, stderr)
+import System.Exit(exitFailure, exitSuccess)
 
 main = do [input, output] <- getArgs
           dat <- parseFile input
           case dat of
-            Right dat -> encodeFile output (dat :: Data.STAR.Type.STAR)
-            Left  err -> do hPutStrLn stderr $ "Error parsing " ++ input ++ ": "
-                            hPrint stderr err
+            Right dat -> do encodeFile output (dat :: Data.STAR.Type.STAR)
+                            exitSuccess
+            Left  err -> do hPutStr   stderr $ "Error parsing " ++ input ++ ": "
+                            hPutStrLn stderr err
+                            exitFailure
+
 
