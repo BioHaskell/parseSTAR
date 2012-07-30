8c668b57a8e44df1b4f55b3fa5bfd0afaa0b2cca
Michal J. Gajda <mjgajda@gmail.com>
2012-07-30 10:54:18 +0200

    Added "face" module Data.STAR for easier import.

---
+++ Data/STAR.hs
@@ -0,0 +1,7 @@
+module Data.STAR where
+
+import qualified Data.STAR.Parser(parse, parseFile)
+import Data.STAR.Type
+import Data.STAR.ChemShifts(extractChemShifts)
+import Data.STAR.Coords    (extractCoords)
+
