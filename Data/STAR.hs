8b8c4f770cfa5445d00127124ad89a5f324524c1
Michal J. Gajda <mgajda@gwdg.de>
2012-07-31 10:19:06 +0200

    Improved dependencies in CABAL file.

--- Data/STAR.hs
+++ Data/STAR.hs
@@ -1,7 +1,19 @@
-module Data.STAR where
+{-# LANGUAGE DisambiguateRecordFields #-}
+module Data.STAR(Data.STAR.Type.STAR(..),
+                 Data.STAR.Type.STARBlock(..),
+                 Data.STAR.Type.STAREntry(..),
+                 Data.STAR.Type.STARKey(..),
+                 parseSTAR,
+                 parseSTARFile,
+                 Data.STAR.ChemShifts.extractChemShifts,
+                 Data.STAR.Coords.extractCoords) where
 
 import qualified Data.STAR.Parser(parse, parseFile)
 import Data.STAR.Type
-import Data.STAR.ChemShifts(extractChemShifts)
-import Data.STAR.Coords    (extractCoords)
+import Data.STAR.ChemShifts(extractChemShifts, ChemShift(..))
+import Data.STAR.Coords    (extractCoords,     Coord    (..))
+
+parseSTAR         = Data.STAR.Parser.parse
+parseSTARFile     = Data.STAR.Parser.parseFile
+
 
