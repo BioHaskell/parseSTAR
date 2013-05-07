92b3c14ea1c4e614c4a3f4b4b3d12f26183e1a2c
Michal J. Gajda <mjgajda@gmail.com>
2013-05-07 11:44:34 +0200

    STAR2Seq: better help, FASTA output.

--- example/STAR2Seq.hs
+++ example/STAR2Seq.hs
@@ -1,37 +1,42 @@
 {-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
 module Main where
 
-import           System.IO             (putStrLn,hPutStrLn,hPrint,stderr)
+import           System.IO             (putStrLn, hPutStrLn, hPrint, stderr)
 import           System.Environment    (getArgs)
 import           Control.Monad         (forM, when)
-import           Data.List             (nub)
+import           Data.List             (nub, intercalate, groupBy)
 import qualified Data.ByteString.Char8 as BS
 
 import           Data.STAR.ResidueCodes
 import           Data.STAR.ChemShifts  (parse, ChemShift(..))
 
 extractSequenceFromChemShifts :: [ChemShift] -> [Char]
-extractSequenceFromChemShifts = map (seqCode . fst) .
-                                fillGaps            .
-                                nub                 .
+extractSequenceFromChemShifts = intercalate "*"            .
+                                map (map $ seqCode . fst3) .
+                                groupBy third              .
+                                fillGaps                   .
+                                nub                        .
                                 map extract
   where
-    extract cs = (comp_id cs, seq_id cs)
-    fillGaps ((a,i):(b,j):rs) | i + 1 >= j = (a,i):fillGaps (          (b,j):rs)
-    fillGaps ((a,i):(b,j):rs)              = (a,i):fillGaps (("-",i+1):(b,j):rs)
-    fillGaps rs                            = rs
-    seqCode "-" = '-'
-    seqCode x   = toSingleLetterCode x
+    extract cs = (comp_id cs, seq_id cs, entity_id cs)
+    fillGaps ((a,i,e):(b,j,f):rs) |     e /= f = (a,i,e):fillGaps (            (b,j,f):rs)
+    fillGaps ((a,i,e):(b,j,f):rs) | i + 1 >= j = (a,i,e):fillGaps (            (b,j,f):rs)
+    fillGaps ((a,i,e):(b,j,f):rs)              = (a,i,e):fillGaps (("-",i+1,e):(b,j,f):rs)
+    fillGaps                  rs               = rs
+    third (_, _, a) (_, _, b) = a == b
+    fst3    (a, _, _) = a
+    seqCode "-"       = '-'
+    seqCode x         = toSingleLetterCode x
         
     -- TODO: check monotonicity of sequence numbers.
     -- TODO: UNK -> '-'
 
 main = do fnames <- getArgs
           when (null fnames) $ do
-            putStrLn "Usage: CheckSeq <input.str> ..."
+            putStrLn "Usage: STAR2Seq <input.str> ..."
             putStrLn "Shows FASTA sequence read from NMR-STAR file."
           forM fnames $ \fname ->
-            do putStr $ fname ++ ": "
+            do putStr $ concat [">", fname, "\n"]
                cs <- Data.STAR.ChemShifts.parse fname
                either (hPutStrLn stderr                        )
                       (putStrLn . extractSequenceFromChemShifts) cs
